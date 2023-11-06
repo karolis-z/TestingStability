412e48eb03c8a822591f9f44de8e532b95a90352
karolis.zaveckas <vis_kaz@seas-nve.dk>
2023-11-06 20:45:35 +0200

    Compiler report plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     kotlin("kapt")
     id("com.google.dagger.hilt.android")
+    id("dev.shreyaspatil.compose-compiler-report-generator")
 }
 
 android {
