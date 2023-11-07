9568c752bc71377b180090fc7099ba884d7a23a9
karolis.zaveckas <vis_kaz@seas-nve.dk>
2023-11-07 10:35:38 +0200

    Made the delay fixed so that we can accurately compare recompositions.

--- app/src/main/java/com/example/myapplication/SettingsDeviceListViewModel.kt
+++ app/src/main/java/com/example/myapplication/SettingsDeviceListViewModel.kt
@@ -40,7 +40,8 @@ class SettingsDeviceListViewModel @Inject constructor(
                 emitAll(
                     locations.toDeviceListItem()
                         .map { device ->
-                            getNetworkStatusUseCase(device.heartnetworkIdentifier)
+                            val delay = device.heartnetworkIdentifier.deviceId.takeLast(1).toIntOrNull()
+                            getNetworkStatusUseCase(device.heartnetworkIdentifier, delay?.times(2) ?: 2)
                                 .asResult()
                                 .map(::mapNetworkConfigurationResultToNetworkState)
                                 .map {
